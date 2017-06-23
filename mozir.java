04aab9d87618054b930904c5d846fda0d210a913
ShotaroChiba7 <shotaro.chiba@excite-software.net>
2017-06-23 16:55:13 +0900

    文字操作テスト

---
+++ mozir.java
@@ -0,0 +1,10 @@
+class mozir{
+public static void main(String Args[]){
+
+String s1 = "aaa1";
+String s2 = "bbb2";
+String s = s1.concat(s2);
+
+System.out.println(s1 + "\n" + s2 + "\n" + s);
+}
+} 
